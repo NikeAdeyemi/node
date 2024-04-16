b10c0e34b3f968b1fc0a6204a6819cb6fa79ef01
Adenike Adeyemi <nikkystarry@gmail.com>
2024-04-16 11:41:59 +0100

    node sychronous and asynchronous

---
+++ index.js
@@ -0,0 +1,38 @@
+const fs = require("fs")
+
+const idris = fs.readFileSync('./files/idris.txt', 'utf8')
+console.log(idris)
+
+
+const meContent = fs.readFileSync('./files/me.txt', 'utf8')
+console.log(meContent)
+
+const cinnamon = "Emco super oat biscuits"
+fs.writeFileSync('./files/christianah.txt', cinnamon)
+
+const sugar = '33% oat flakes, no sugar added'
+fs.writeFileSync('./files/biscuit.txt', sugar)
+
+fs.readFile('./files/asyncfile.txt', 'utf8', (err, data) => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log(data)
+    }
+})
+
+fs.readFile('./files/asyncc.txt', 'utf8', (e, data) => {
+    if(e){
+        console.log(e)
+    }else{
+        console.log(data)
+    }
+})
+
+fs.readFile('./files/asyncfileagain.txt', 'utf8', (error, data) => {
+    if(error){
+        console.log(error)
+    }else{
+        console.log(data)
+    }
+})
